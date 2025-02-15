e3d8534548ff7375e152692fbd752030cb4a3687
Artem <kudrinskiy@internet.ru>
2025-02-16 00:45:37 +0300

    Configure transitions between screens in AuthorizationActivity

--- app/src/main/java/com/application/boardwise/AuthorizationActivity.kt
+++ app/src/main/java/com/application/boardwise/AuthorizationActivity.kt
@@ -4,42 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.application.boardwise.ui.theme.BoardWiseTheme
+import com.application.boardwise.ui.theme.DarkBlue
 
-class MainActivity : ComponentActivity() {
+class AuthorizationActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BoardWiseTheme {
-                Surface(onClick = { /*TODO*/ }) {
-                    
+                Surface(color = DarkBlue) {
+                    AuthApp()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BoardWiseTheme {
-        Greeting("Android")
+    @Composable
+    fun AuthApp() {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "login") {
+            composable("login") { LoginScreen(navController = navController) }
+            composable("register") { RegisterScreen(navController = navController) }
+        }
     }
 }
