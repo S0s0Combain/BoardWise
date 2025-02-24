90a685f7a7cf1b4807a367d9fd2776ea70cd67df
Artem <kudrinskiy@internet.ru>
2025-02-24 17:48:12 +0300

    Change the main authorization activity container to Scaffold

--- app/src/main/java/com/application/boardwise/AuthorizationActivity.kt
+++ app/src/main/java/com/application/boardwise/AuthorizationActivity.kt
@@ -5,9 +5,14 @@ import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -23,19 +28,19 @@ class AuthorizationActivity : ComponentActivity() {
             BoardWiseTheme {
                 val window: Window = this.window
                 window.navigationBarColor = MaterialTheme.colorScheme.background.toArgb()
-                Surface(color = DarkBlue) {
-                    AuthApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPaddings ->
+                    AuthApp(innerPaddings)
                 }
             }
         }
     }
+}
 
-    @Composable
-    fun AuthApp() {
-        val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = "login") {
-            composable("login") { LoginScreen(navController = navController) }
-            composable("register") { RegisterScreen(navController = navController) }
-        }
+@Composable
+fun AuthApp(innerPaddings: PaddingValues) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "login", modifier = Modifier.fillMaxSize().padding(innerPaddings)) {
+        composable("login") { LoginScreen(navController = navController) }
+        composable("register") { RegisterScreen(navController = navController) }
     }
 }
